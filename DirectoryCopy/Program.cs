0f47d1e1dc7ca17da89ee8c59705e90f502ddf77
John Hinz <jhinz402@hotmail.com>
2021-12-26 00:17:06 -0500

    Changes to DirectoryCopy

--- DirectoryCopy/Program.cs
+++ DirectoryCopy/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Polly.Retry;
+using Serilog;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -14,11 +16,16 @@ namespace DirectoryCopy
 
         static void Main(string[] args)
         {
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
             var configuration = builder.Build();
             ConfigurationBinder.Bind(configuration.GetSection("AppSettings"), appSettings);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("consoleapp.log")
+                .CreateLogger();
 
             _fileAccessRetryPolicy = Policy
                 .Handle<FileLoadException>()
@@ -29,18 +36,38 @@ namespace DirectoryCopy
                 .WaitAndRetry(retryCount: 5, retryNumber => TimeSpan.FromMilliseconds(5000));
 
 
-            string[] files = Directory.GetFiles(appSettings.SourceDirectory, "*.*", SearchOption.AllDirectories);
+            //string[] files = Directory.GetFiles(appSettings.SourceDirectory, "*.pdf", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(appSettings.SourceDirectory, "*.jpg", SearchOption.AllDirectories);
 
             Parallel.ForEach(files, (f => {
                 try
                 {
                     string[] fileParts = f.Split('\\');
                     string fileName = fileParts[fileParts.Length - 1];
-                    _fileAccessRetryPolicy.Execute(() => { File.Copy(f, $"{appSettings.TargetDirectory}\\{fileName}", true); });
-                }
-                catch
-                {
 
+                    string[] matchExisting = Directory.GetFiles(appSettings.TargetDirectory, fileName);
+                    if (matchExisting.Length > 0)
+                    {
+                        Log.Warning($"Found duplicate in target directory: {fileName}");
+                    }
+                    while (matchExisting.Length > 0)
+                    {
+                        string[] newfileNames = fileName.Split('.');
+                        string newfileName = string.Empty;
+                        for (int x = 0; x < newfileNames.Length - 1; x++)
+                        {
+                            newfileName = $"{newfileNames[x]}";
+                        }
+                        fileName = $"{newfileName}_1.{newfileNames[newfileNames.Length - 1]}";
+                        matchExisting = Directory.GetFiles(appSettings.TargetDirectory, fileName);
+                    }
+
+                    _fileAccessRetryPolicy.Execute(() => { File.Copy(f, $"{appSettings.TargetDirectory}\\{fileName}", true); });
+                    Log.Information($"Copied file: {f} to {appSettings.TargetDirectory}\\{fileName}");
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"Error: {ex.Message}");
                 };
             }));
 
